5a1761e43fb72ccccf2dbed3e4439493d88c84f6
Lucas Henrique Da Silva <lucashbsj@outlook.com>
2024-05-06 18:24:09 +0100

    created a file

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CS_Calculate_Print_School_Grades.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
